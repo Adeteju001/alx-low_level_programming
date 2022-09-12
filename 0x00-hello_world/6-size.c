0619a731b1703f2ee01dd967ba239343b33119f9
Adeteju001 <adegokeadeteju001@gmail.com>
2022-09-12 02:05:01 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -15,4 +15,4 @@ int main(void)
 	printf("size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", (int) sizeof(float));
 	return (0);
-}}
+}
