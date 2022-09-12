2c7d697214e0f725a162e799d031a6cca31c5232
Adeteju001 <adegokeadeteju001@gmail.com>
2022-09-12 02:58:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<unistd.h>
 /**
- * main - prints a standard error
- *
- * Return: Always 1 (success)
- */
+ * main - Entry point
+ * Description: Print a standard error
+ * Return: Always 1 (Success)
+ **/
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-fprintf(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-return (1);
+fprintf(stderr,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return(1);
