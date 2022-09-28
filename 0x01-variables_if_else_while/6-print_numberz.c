f2927b066574781365067dd09e93c0f32c768b37
Adeteju001 <adegokeadeteju001@gmail.com>
2022-09-28 09:06:17 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry mode
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9' n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}	
