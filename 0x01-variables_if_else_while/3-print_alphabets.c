77584400a2655f82d95c1f76beb3ddb7f3834781
Adeteju001 <adegokeadeteju001@gmail.com>
2022-09-28 06:35:53 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main(void)
 
 	char A;
 
-	for (a= 'a'; a <= 'z'; a++)
+	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
-	for (A= 'A'; A <= 'Z'; A++)
+	for (A = 'A'; A <= 'Z'; A++)
 		putchar(A);
 	putchar('\n');
 	return (0);
