1d1b9d1e722788d14a496f92b76d70af69160991
Adeteju001 <adegokeadeteju001@gmail.com>
2022-09-28 07:53:44 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry mode
+ * Return: returns 0
+ */
+int main(void)
+{
+	char num; 
+	for (num = 0; num < 10; num++)
+		printf("%d" , num"\n");
+	return (0);
+}
