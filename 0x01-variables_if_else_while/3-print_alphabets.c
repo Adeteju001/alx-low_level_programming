6ecbf2a0e6865cf7544e62fc39f5bd648aee9f15
Adeteju001 <adegokeadeteju001@gmail.com>
2022-09-28 05:44:23 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry mode
+ * Return: returns 0
+ */
+int main(void)
+{
+	char a;
+
+	char A;
+
+	for (a= 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (A= 'A'; A <= 'Z'; A++)
+		putchar(A);
+	return (0);
+}
