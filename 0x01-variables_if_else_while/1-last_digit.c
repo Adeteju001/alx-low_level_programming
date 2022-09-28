d2ba257bb13b4d5e0d1fed4153c264358cf77e47
Adeteju001 <adegokeadeteju001@gmail.com>
2022-09-28 04:41:01 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main()
 	if ((n % 10) == 0)
 		printf("%d and is 0\n", (n % 10));
 
-	if ((n % 10) < 6)
+	if ((n % 10) < 6 && (n % 10) ! = 0)
 		printf("%d and is less than 6 and not 0\n", (n % 10));
 	return(0);
 }
