8390c17a7b9214398b16a09a5a63360671dfc464
Adeteju001 <adegokeadeteju001@gmail.com>
2022-09-28 06:59:07 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry mode
+ * Return: returns 0
+ */
+int main(void)
+{
+	char alphabt;
+	for (a = 'a'; a <= 'z'; a++)
+		if(alphabt == 'e' || alphabt == 'q')
+			continue;
+	putchar(alphabt);
+	putchat('\n');
+	return (0);
+}
