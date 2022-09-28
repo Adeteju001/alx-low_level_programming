84bb66df91e2eaf9edff99aa05d77b3a4a68b307
Adeteju001 <adegokeadeteju001@gmail.com>
2022-09-28 09:23:24 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry mode
+ * Return: returns 0
+ */
+int main(void)
+{
+	char alpha
+
+		for (alpha = 'z'; alpha >= 'a'; alpha--)
+			putchar(alpha);
+	putchar('\n');
+	return (0);
+}
