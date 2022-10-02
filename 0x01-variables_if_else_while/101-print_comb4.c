c5c53a04379fc4c8165a4050f1026968a2395f60
Adeteju001 <adegokeadeteju001@gmail.com>
2022-10-02 04:39:14 -0700

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	char a, b, c;
 
-	for (a = '0' a <= '7' a++)
+	for (a = '0'; a <= '7'; a++)
 	{
-		for (b = a+1; b <= '8' b++)
+		for (b = a+1 b <= '8'; b++)
 		{
-			for (c = b+1; c <= '9' c++)
+			for (c = b+1 c <= '9'; c++)
 			{
 				if (a == b)
 					continue;
 				putchar(a);
 				putchar(b);
 				putchar(c);
-				if (a == '7' && b == '8' && c == '9')
+				if (a == '7'; && b == '8'; && c == '9')
 					continue;
 				putchar(',');
 				putchar(' ');
