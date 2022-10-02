d2c1648c3c407307a135688405ab78df603bd853
Adeteju001 <adegokeadeteju001@gmail.com>
2022-10-02 02:29:32 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry code
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a,b
+
+		for (a = '0'; a <= '8'; a++)
+		{
+			for (b = a + 1; b <= '9' b++)
+			{
+				if ( a == b)
+					continue;
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
