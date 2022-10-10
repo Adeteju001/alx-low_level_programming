5a011ab69c1658aea485541276755e442ed91877
Adeteju001 <adegokeadeteju001@gmail.com>
2022-10-10 05:25:05 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main entry code
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("_putchar");
+	putchar ('\n');
+	return (0);
+}
