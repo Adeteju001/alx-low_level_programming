da8f43e3e36290173c7d06ef1130e8d08850b9c7
Adeteju001 <adegokeadeteju001@gmail.com>
2022-10-13 02:35:20 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 /**
  * main - entry mode
- * Return: returns 0
+ * Return: Always 0 (Success)
  */
 int main(void)
-{ 
-	         putchar('_'); 
-	          putchar('p'); 
-	           putchar('u'); 
-	            putchar('t'); 
-	             putchar('c'); 
-	              putchar('h'); 
-	               putchar('a'); 
-	                putchar('r'); 
-		         putchar('\n'); 
-		          return (0); 
-		   }
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
