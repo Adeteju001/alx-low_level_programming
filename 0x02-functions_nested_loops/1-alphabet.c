d47e1fe5b1d4c853870e4b129e2ad4c88a8bf48f
Adeteju001 <adegokeadeteju001@gmail.com>
2022-10-13 02:41:40 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+/**
+ *print_alphabet - Entry mode
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
