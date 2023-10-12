31f63b7e91a7a4c8ddc3c539506d8b1d8346c8c8
Adeteju001 <adegokeadeteju001@gmail.com>
2023-10-12 03:51:57 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,13 +11,13 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	/**
-	 * i = 0;
-	 *
-	 * while (i < 1)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
+	* i = 0;
+	*
+	* while (i < 1)
+	* {
+	* putchar(i);
+	* }
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
