615aa448d8aa0297cb207d8dd851a27882dfb654
Adeteju001 <adegokeadeteju001@gmail.com>
2023-10-16 20:21:43 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- *void reset_to_98 - function
- *@n: parameters
+ * reset_to_98 - function
+ * @n: parameters
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
